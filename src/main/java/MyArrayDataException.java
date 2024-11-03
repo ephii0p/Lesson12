1951ad9d9063b0e3b763bfee4eac49ac914cc13f
ephii0p <belunkind@gmail.com>
2024-11-03 15:07:28 +0400

    Initial commit

---
+++ src/main/java/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+public class MyArrayDataException extends RuntimeException {
+  public MyArrayDataException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+public class MyArraySizeException extends RuntimeException {
+  public MyArraySizeException(String message) {
+    super(message);
+  }
+}
